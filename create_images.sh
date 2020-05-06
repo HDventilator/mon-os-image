108175261b6e7789ae765d6b08957e2e5fe767d6
Helmut Jacob <helmut.jacob@rommelag.com>
2020-05-06 15:11:04 +0200

    Add overlay stage for hdvent specific installation

--- create_images.sh
+++ create_images.sh
@@ -1,5 +1,9 @@
 
-# get pi-gen
+# get pi-gen if not here yet
 [ ! -d pi-gen ] && git clone https://github.com/RPi-Distro/pi-gen.git
 
+# add all overlay stages
+cp -r overlay/* pi-gen
+
+# let's go
 pi-gen/build-docker.sh -c config
