b322470ba9e6e95975d3cad654fb433017307e45
Helmut Jacob <helmut.jacob@rommelag.com>
2020-05-07 17:46:10 +0200

    Start frontend as service

--- overlay/stage_hdvent/05-frontend/01-run.sh
+++ overlay/stage_hdvent/05-frontend/01-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
+install -v -m 644 files/hdvent_frontend.service "${ROOTFS_DIR}/etc/systemd/system/"
+
 on_chroot << EOF
 pip3 install git+https://github.com/HDventilator/mon-frontend@deploy
+systemctl enable hdvent_frontend
 EOF
 
