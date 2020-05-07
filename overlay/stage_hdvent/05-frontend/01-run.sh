62f2e2e337c0fcf29d23b31862893aa6b328a5dc
Helmut Jacob <helmut.jacob@rommelag.com>
2020-05-07 19:38:25 +0200

    Switch mon frontend to master branch

--- overlay/stage_hdvent/05-frontend/01-run.sh
+++ overlay/stage_hdvent/05-frontend/01-run.sh
@@ -3,7 +3,7 @@
 install -v -m 644 files/hdvent_frontend.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 on_chroot << EOF
-pip3 install git+https://github.com/HDventilator/mon-frontend@deploy
+pip3 install git+https://github.com/HDventilator/mon-frontend@master
 systemctl enable hdvent_frontend
 EOF
 
