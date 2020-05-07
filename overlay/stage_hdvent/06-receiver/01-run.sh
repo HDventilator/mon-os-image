b0b36e694a855c2655d47b2da574bab266885675
Helmut Jacob <helmut.jacob@rommelag.com>
2020-05-07 17:40:05 +0200

    Start HDVent receiver as service

--- overlay/stage_hdvent/06-receiver/01-run.sh
+++ overlay/stage_hdvent/06-receiver/01-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
+install -v -m 644 files/hdvent_receiver.service "${ROOTFS_DIR}/etc/systemd/system/"
+
 on_chroot << EOF
 pip3 install git+https://github.com/HDventilator/mon-receiver.git@master
+systemctl enable hdvent_receiver
 EOF
 
