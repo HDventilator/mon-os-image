33aeaccd79ef22dd40d74e2cd44ccc9e51fd3c5c
Martin Ulmschneider <7497782+mulmschneider@users.noreply.github.com>
2020-05-14 22:12:04 +0200

    WIP

--- overlay/build-docker.sh
+++ overlay/build-docker.sh
@@ -76,37 +76,36 @@ fi
 BUILD_OPTS="$(echo "${BUILD_OPTS:-}" | sed -E 's@\-c\s?([^ ]+)@-c /config@')"
 
 ${DOCKER} build -t pi-gen "${DIR}"
-#if [ "${CONTAINER_EXISTS}" != "" ]; then
-#	trap 'echo "got CTRL+C... please wait 5s" && ${DOCKER} stop -t 5 ${CONTAINER_NAME}_cont' SIGINT SIGTERM
-#	time ${DOCKER} run --rm --privileged \
-#		--volume "${CONFIG_FILE}":/config:ro \
-#		-e "GIT_HASH=${GIT_HASH}" \
-#		--volumes-from="${CONTAINER_NAME}" --name "${CONTAINER_NAME}_cont" \
-#		pi-gen \
-#		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
-#	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
-#	rsync -av work/*/build.log deploy/" &
-#	wait "$!"
-#else
-#	trap 'echo "got CTRL+C... please wait 5s" && ${DOCKER} stop -t 5 ${CONTAINER_NAME}' SIGINT SIGTERM
-#	time ${DOCKER} run --name "${CONTAINER_NAME}" --privileged \
-#		--volume "${CONFIG_FILE}":/config:ro \
-#		--volume "${CACHE_DIR}":/pi-gen/work/:rw \
-#		-e "GIT_HASH=${GIT_HASH}" \
-#		pi-gen \
-#		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
-#	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
-#	rsync -av work/*/build.log deploy/" &
-#	wait "$!"
-#fi
-#echo "copying results from deploy/"
-#${DOCKER} cp "${CONTAINER_NAME}":/pi-gen/deploy .
-#ls -lah deploy
-#
-## cleanup
-#if [ "${PRESERVE_CONTAINER}" != "1" ]; then
-#	${DOCKER} rm -v "${CONTAINER_NAME}"
-#fi
-#
-#echo "Done! Your image(s) should be in deploy/"
-echo "###########FIN##############"
+if [ "${CONTAINER_EXISTS}" != "" ]; then
+	trap 'echo "got CTRL+C... please wait 5s" && ${DOCKER} stop -t 5 ${CONTAINER_NAME}_cont' SIGINT SIGTERM
+	time ${DOCKER} run --rm --privileged \
+		--volume "${CONFIG_FILE}":/config:ro \
+		-e "GIT_HASH=${GIT_HASH}" \
+		--volumes-from="${CONTAINER_NAME}" --name "${CONTAINER_NAME}_cont" \
+		pi-gen \
+		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
+	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
+	rsync -av work/*/build.log deploy/" &
+	wait "$!"
+else
+	trap 'echo "got CTRL+C... please wait 5s" && ${DOCKER} stop -t 5 ${CONTAINER_NAME}' SIGINT SIGTERM
+	time ${DOCKER} run --name "${CONTAINER_NAME}" --privileged \
+		--volume "${CONFIG_FILE}":/config:ro \
+		--volume "${CACHE_DIR}":/pi-gen/work/:rw \
+		-e "GIT_HASH=${GIT_HASH}" \
+		pi-gen \
+		bash -e -o pipefail -c "dpkg-reconfigure qemu-user-static &&
+	cd /pi-gen; ./build.sh ${BUILD_OPTS} &&
+	rsync -av work/*/build.log deploy/" &
+	wait "$!"
+fi
+echo "copying results from deploy/"
+${DOCKER} cp "${CONTAINER_NAME}":/pi-gen/deploy .
+ls -lah deploy
+
+# cleanup
+if [ "${PRESERVE_CONTAINER}" != "1" ]; then
+	${DOCKER} rm -v "${CONTAINER_NAME}"
+fi
+
+echo "Done! Your image(s) should be in deploy/"
