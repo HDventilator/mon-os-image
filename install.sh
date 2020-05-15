a33364b5ec2bb510a9dfdbb55e4653dbd52a4428
Martin Ulmschneider <7497782+mulmschneider@users.noreply.github.com>
2020-05-15 06:46:12 +0200

    WIP

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt-get -y update && \
-apt-get -y install --no-install-recommends \
+sudo apt-get -y update && \
+sudo apt-get -y install --no-install-recommends \
     git vim parted \
     quilt coreutils qemu-user-static debootstrap zerofree zip dosfstools \
     bsdtar libcap2-bin rsync grep udev xz-utils curl xxd file kmod bc\
