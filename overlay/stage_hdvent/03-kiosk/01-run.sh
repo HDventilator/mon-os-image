5af98d192aa02deb8bc611646582539d9c15e5aa
Helmut Jacob <helmut.jacob@rommelag.com>
2020-05-18 08:23:10 +0200

    Fix potential permission problem in ~/.config
    /home/pi/.config is owned by root in the current image preventing proper
    execution of the autostart script.
    
    Fix this by individually creating all subdirectories with the right
    permissions.

--- overlay/stage_hdvent/03-kiosk/01-run.sh
+++ overlay/stage_hdvent/03-kiosk/01-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/lxsession"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/lxsession/LXDE"
 install -v -o 1000 -g 1000 -m 644 files/autostart "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/lxsession/LXDE/"
